30d9d49871ba3fa7a16885b2cc7548640b4d109a
Scott Wagner <scottywags16@gmail.com>
2018-02-07 21:46:08 -0500

    add infrastructure for using typography.js

---
+++ src/utils/typography.js
@@ -0,0 +1,6 @@
+import Typography from 'typography';
+import typographyThemeMoraga from 'typography-theme-moraga';
+
+const typography = new Typography(typographyThemeMoraga);
+
+export default typography;
