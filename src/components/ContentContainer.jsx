051cc4132dbdf40931f7c23fe13d76a772385b42
Scott Wagner <scottywags16@gmail.com>
2018-02-10 21:06:41 -0500

    tweak design on content container
    make the container a bit more narrow
    pads the content
    add a small border radius

--- src/components/ContentContainer.jsx
+++ src/components/ContentContainer.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ContentContainer = ({ children }) => (
-  <div style={{ backgroundColor: 'white', width: '80%', margin: '0 auto' }}>
-    {children()}
-  </div>
-);
+const ContentContainer = ({ children }) => {
+  const contentStyle = {
+    backgroundColor: 'white',
+    width: '70%',
+    margin: '0 auto',
+    paddingLeft: '5px',
+    paddingRight: '5px',
+    borderRadius: '3px',
+  };
+
+  return (
+    <div style={contentStyle}>
+      {children()}
+    </div>
+  );
+};
 
 ContentContainer.propTypes = {
   children: PropTypes.func.isRequired,
