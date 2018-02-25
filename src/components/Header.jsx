237e77a6620e5c2020a8adcf53174beff79fd88b
Scott Wagner <scottywags16@gmail.com>
2018-02-25 17:40:52 -0500

    simplify header and add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import Link from 'gatsby-link';
-import HeaderLinksList from './HeaderLinksList';
+import logo from '../../images/logo.jpg';
 
 const headerStyle = {
-  height: '50px',
-  display: 'grid',
-  gridTemplateColumns: 'auto auto',
+  height: 'auto',
+  margin: 'auto',
   backgroundColor: 'hsl(210, 9%, 96%)',
+};
+
+const imageStyle = {
+  margin: 'auto',
+  display: 'block',
+  borderRadius: '100px',
 };
 
 const Header = () => (
   <div style={headerStyle}>
-    <h3 style={{ marginLeft: '1rem' }}>
-      <Link to="/" style={{ textShadow: 'none' }}>
-          Scott Wagner
-      </Link>
-    </h3>
-    <HeaderLinksList
-      style={{ alignSelf: 'center', justifySelf: 'end' }}
-    />
+    <Link to="/">
+      <img
+        src={logo}
+        style={imageStyle}
+        alt="logo"
+      />
+    </Link>
   </div>
 );
 
