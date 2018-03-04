52fc15903eeb30f8e33d56c2ac444e3b670b292d
Scott Wagner <scottywags16@gmail.com>
2018-03-03 20:47:52 -0500

    move deploy script to top level

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 aws s3 rm s3://johnscottwagner.com --recursive
 terraform apply -auto-approve
-aws s3 cp public/ s3://johnscottwagner.com --recursive
+aws s3 cp ../public/ s3://johnscottwagner.com --recursive
