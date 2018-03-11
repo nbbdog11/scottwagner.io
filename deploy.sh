91cac7c388082bba9180c6c8f01465dbdc68928a
Scott Wagner <scottywags16@gmail.com>
2018-03-11 12:36:09 -0400

    add cloudfront cache invalidation to deploy process

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 aws s3 rm s3://johnscottwagner.com --recursive
+aws cloudfront create-invalidation --distribution-id E8BNTDZLV8Z2T --paths "/*"
 terraform apply -auto-approve deploy/
 aws s3 cp public/ s3://johnscottwagner.com --recursive
