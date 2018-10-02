431684a335d1e2c29cf881951f23ef06b56d190c
Scott Wagner <scottywags16@gmail.com>
2018-10-02 00:19:08 -0400

    fix deploy script terraform issue

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 aws s3 rm s3://johnscottwagner.com --recursive
 aws cloudfront create-invalidation --distribution-id E8BNTDZLV8Z2T --paths "/*"
-terraform apply -auto-approve deploy/
+pushd deploy/
+terraform apply -auto-approve
+popd
 aws s3 cp public/ s3://johnscottwagner.com --recursive
