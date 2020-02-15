1d9d7f3899fedf6634d5c5c49346046aa6210ddb
Scott Wagner <scottywags16@gmail.com>
2020-02-14 22:13:38 -0500

    add gatsby plugin for styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-typescript',
     'gatsby-transformer-remark',
+    'gatsby-plugin-styled-components',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
