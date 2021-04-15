f59c42cde83216694776ae0053dfe3cf61d58865
Scott Wagner <scottywags16@gmail.com>
2021-04-15 03:21:59 -0400

    update deploy script with next output folder

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ aws cloudfront create-invalidation --distribution-id E1Z1DCSMC5LN0O --paths "/*"
 pushd deploy/
 terraform apply -auto-approve
 popd
-aws s3 cp public/ s3://scottwagner.io --recursive
+aws s3 cp out/ s3://scottwagner.io --recursive
