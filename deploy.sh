078f8697e17fd42eaeded8e37176bb91814dc2f5
Scott Wagner <scottywags16@gmail.com>
2021-05-26 21:15:00 -0400

    remove code related to manual deploy

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-aws s3 rm s3://scottwagner.io --recursive
-aws cloudfront create-invalidation --distribution-id E1Z1DCSMC5LN0O --paths "/*"
-pushd deploy/
-terraform apply -auto-approve
-popd
-aws s3 cp out/ s3://scottwagner.io --recursive
